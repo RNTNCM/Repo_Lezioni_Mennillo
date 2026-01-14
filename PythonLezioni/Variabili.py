abfa49c0918c57eb7d5624a26fc7742ce18dda92
RNTNCM <renato.anicama@edu.immaginazioneelavoro.it>
2026-01-14 14:02:45 +0100

    1A

--- PythonLezioni/Variabili.py
+++ PythonLezioni/Variabili.py
@@ -2,4 +2,4 @@ mioNome = "Renato"
 miaEta = 17
 corso = "Tecnico Informatico 2025/26"
 
-print(f"Ciao, mi chiamo {mioNome}")
+print("Ciao, mi chiamo", mioNome, "ho", miaEta, "anni e sono uno studente del corso", corso)
